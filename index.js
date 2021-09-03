a6bcdcc8a010658ad8921c96c6265e641eb2ad6e
Mike Thomas <mthomas.theadvocate@gmail.com>
2021-09-02 18:14:37 -0700

    Index (Update)

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const fs = require("fs");
 const path = require("path");
 const inquirer = require("inquirer");
 const generateMarkdown = require("./utils/generateMarkdown");
+const { rejects } = require("assert");
+const { resolve } = require("path");
 
 // array of questions for user
 const questions = [
@@ -11,11 +13,72 @@ const questions = [
     messages: "What is your Github username?", 
 
    },
-    
+   {
+    type: "input",
+    name:"email",
+    messages: "What is your email address?", 
+
+   },
+   {
+    type: "input",
+    name:"title",
+    messages: "What is your project's name?", 
+
+   },
+   {
+    type: "input",
+    name:"description",
+    messages: "Please write a short description of your project", 
+
+   },
+   {
+    type: "input",
+    name:"license",
+    messages: "What kind of license should your project have?",
+    choices: ["MIT","APACHE 2.0","GPL 3.0","BSD 3", "None"] 
+
+   },
+   {
+    type: "input",
+    name:"installation",
+    messages: "What command should be run to onstall dependencies?", 
+
+   },
+   {
+    type: "input",
+    name:"test",
+    messages: "What command should be run to run tests?", 
+
+   },
+   {
+    type: "input",
+    name:"usage",
+    messages: "What does the user need to know abotu the repo?", 
+
+   },
+   {
+    type: "input",
+    name:"contributing",
+    messages: "What does the user need to know about contributing to the repo?", 
+
+   }
 ];
 
+
 // function to write README file
-function writeToFile(fileName, data) {
+
+
+//tdb fs.writeFileSync(.... filename, data);
+function writeToFile(fileName, data, path) {
+    return new Promise((fileName,data,path) => {
+        fs.writeFileSync(path(fileName),data, path);
+        
+        
+       if (path) {
+           rejects(path);
+           return;
+       }
+    });
 }
 
 // function to initialize program
@@ -23,8 +86,8 @@ function init() {
 
     inquirer.prompt(questions)
     .then((inquirerResponses) => {
-        console.log("Generationg Readme...");
-        writeToFile("README.md", generateMarkdown);
+        console.log("Generating Readme...");
+        writeToFile("README.md", generateMarkdown({...inquirerResponses}));
     })
 
 }
